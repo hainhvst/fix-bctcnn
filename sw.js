81ca3c480586fc18d30cf41b16469ce93e227207
hainhvst <146146814+hainhvst@users.noreply.github.com>
2026-06-17 09:48:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (e) => {
+    // Để trống cũng được, chỉ cần khai báo cho trình duyệt biết có Service Worker
 });
