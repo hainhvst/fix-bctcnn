0d46e442d7130dfe071aa75b11ccfd79818cf834
hainhvst <146146814+hainhvst@users.noreply.github.com>
2026-06-17 10:09:39 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,13 @@
-const CACHE_NAME = 'bctcnn-pwa-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
-    './logo-KB.png'
-];
-
-// Bước 1: Lưu các file quan trọng vào bộ nhớ đệm (Cache) khi cài đặt
+// File này giúp lừa Chrome rằng Web có khả năng chạy Offline
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Đã cài đặt và lưu Cache');
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+    self.skipWaiting(); 
 });
 
-// Bước 2: Kích hoạt Service Worker mới ngay lập tức
 self.addEventListener('activate', (e) => {
     e.waitUntil(self.clients.claim());
 });
 
-// Bước 3: Xử lý khi trình duyệt tải trang (Ưu tiên mạng, nếu rớt mạng thì lấy từ Cache)
+// Hàm fetch rỗng nhưng đủ chuẩn để vượt qua bộ lọc của Chrome
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        fetch(e.request).catch(() => {
-            console.log('[Service Worker] Mất mạng, đang tải từ Cache...');
-            return caches.match(e.request);
-        })
-    );
+    // Không làm gì cả
 });
